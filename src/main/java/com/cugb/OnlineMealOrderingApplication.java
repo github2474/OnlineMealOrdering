30630c6968922b5456ae219fbac336632e30c685
hexingyun <2474353483@qq.com>
2023-12-21 23:17:56 +0800

    Initial commit

---
+++ src/main/java/com/cugb/OnlineMealOrderingApplication.java
@@ -0,0 +1,13 @@
+package com.cugb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMealOrderingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineMealOrderingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cugb/OnlineMealOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cugb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMealOrderingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
